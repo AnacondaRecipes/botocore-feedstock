fbe0ac162654481ba243db39c9cdbd400e725cab
Andrii Vetroumov <avetroumov@anaconda.com>
2026-09-03 09:18:28 +0300

    Fix review findings

--- recipe/activate.bat
+++ recipe/activate.bat
@@ -3,5 +3,5 @@ setlocal enabledelayedexpansion
 
 rem Set the REQUESTS_CA_BUNDLE environment variable to the path of the cacert.pem file
 rem Needed for the botocore package to use the correct CA bundle.
-set "REQUESTS_CA_BUNDLE=%PREFIX%\Library\ssl\cacert.pem"
+set "REQUESTS_CA_BUNDLE=%CONDA_PREFIX%\Library\ssl\cacert.pem"
 if errorlevel 1 exit /b 1
--- recipe/activate.sh
+++ recipe/activate.sh
@@ -3,4 +3,4 @@ set -ex
 
 # Set the REQUESTS_CA_BUNDLE environment variable to the path of the cacert.pem file
 # Needed for the botocore package to use the correct CA bundle.
-export REQUESTS_CA_BUNDLE=$PREFIX/ssl/cacert.pem
+export REQUESTS_CA_BUNDLE=$CONDA_PREFIX/ssl/cacert.pem
